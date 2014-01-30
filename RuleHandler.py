d94a9f238cef9d2797ce4184c48d1fed747d85a3
Timidger <APragmaticPlace@gmail.com>
2014-01-30 11:57:56 -0600

    General cleanup of RuleHandler. Removed the old unit test. Removed any RuleGenerator dependencies

--- RuleHandler.py
+++ RuleHandler.py
@@ -6,14 +6,13 @@ Created on Fri Dec 28 13:14:57 2012
 @author: Preston
 """
 
-import RuleGenerator
 from Card import Card
 
 class RuleHandler(object):
-    """Holds rules to be executed from input corresponding to triggers"""
+    """Holds rules to be executed from input corresponding to triggers."""
     def __init__(self, rules = None):
-        """Rules should be a list or tuple of rule objects"""
-        self.rules = {} # {rule object: codified script}
+        """Rules should be a list or tuple of rule objects."""
+        self.rules = {} # {rule object: rule script}
         if rules:
             for rule in tuple(rules):
                 self.add_rule(rule)
@@ -21,7 +20,7 @@ class RuleHandler(object):
     def check_rules(self, trigger, check_none = False):
         """Using the trigger as a key, returns a list of the rule scripts
         that pass the check; if check_None is given and True, triggers that
-        are None also get counted"""
+        are None also get counted."""
         rule_scripts = []
         if type(trigger) == Card:
             trigger = trigger.rank, trigger.suit
@@ -41,48 +40,13 @@ class RuleHandler(object):
 
     def add_rule(self, rule):
         """If the rule is not in the rules, adds it to the rules. If the
-        rulewas added, returns True. Else, returns False"""
+        rulewas added, returns True. Else, returns False."""
         if rule not in self.rules:
-            self.rules.update({rule: RuleGenerator.codify(rule)})
+            self.rules.update({rule: rule.script})
             return True
         else:
             return False
 
     def remove_rule(self, rule):
-        """Removes the rule from the list of rules. If the rule is not
-        present, a KeyError is raised"""
+        """Removes the rule from the list of rules."""
         self.rules.pop(rule)
-
-if __name__ == '__main__':
-    import Rule
-    from Pile import Pile
-    RH = RuleHandler({})
-    test_pile = Pile()
-    RuleGenerator.WHITE_LIST.add(test_pile)
-    Cards = tuple(Card(suit, rank) for rank in (
-    "Ace 2 3 4 5 6 7 8 9 10 King Queen Jack".split()) for suit in (
-    "Hearts", "Spades", "Clubs", "Diamonds") )
-    RuleGenerator.WHITE_LIST.add(Cards)
-    fill_pile = Rule.Rule('Fill Pile',  None,
-                            'Set test_pile.cards to Cards')
-    empty = Rule.Rule('Empty deck', ('Correct Phrase',),
-                      'Set test_pile.cards to []')
-    RH.add_rule(fill_pile)
-    RH.add_rule(empty)
-    for rule in RH.check_rules('Test'):
-        eval(rule or '')
-    assert not test_pile.cards
-    for rule in RH.check_rules('Test', True):
-        eval(rule or '')
-    assert len(test_pile.cards) == 52
-    for rule in RH.check_rules('This is a test phrase'):
-        eval(rule or '')
-    assert len(test_pile.cards) == 52
-    for rule in RH.check_rules(Card(None, None)):
-        eval(rule or '')
-    assert len(test_pile.cards) == 52
-    for rule in RH.check_rules('Correct Phrase'):
-        eval(rule or '')
-    assert not test_pile.cards
-    print
-    print 'Rule, RuleGenerator, and RuleHandler tests complete!'
