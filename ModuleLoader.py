351942ce1fff8d7ab4e6ea3afd43f425e5aa6ca4
Preston Carpenter <timidger@thinkpad-crunchbang>
2014-02-12 08:52:56 -0600

    Added the ModuleLoader module. It handles importing python code from the Variant folder

---
+++ ModuleLoader.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2014/02/12 07:37:00
+
+@author: Preston
+"""
+
+import os
+import inspect
+
+def import_module(module_name):
+    """Searches the Variants directory for module_name and returns it"""
+    assert type(module_name) == str
+    try:
+        module = __import__("Variants." + module_name, 
+                            fromlist = ["filler"])
+                            #non-empty list imports module_name
+    except ImportError, e:
+        raise (ImportError, 
+               "Could not find {} in {}".format(module_name, 
+                os.getcwd() + os.sep + "Variants"))
+        
+    return module
+
+def get_functions(module):
+    """Returns a list of the non-hidden functions in module"""
+    return [function for function in module.__dict__.itervalues()
+            if (is_function(function))]
+
+def is_function(function):
+    """Returns true if function is a function"""
+    return inspect.isfunction(function)
