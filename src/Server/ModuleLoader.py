a743fe64dbaf95d17a4ceff2c08eede3714c8e4f
Timidger <APragmaticPlace@gmail.com>
2014-03-26 13:26:42 -0500

    Added load_rules method to RuleHandler

--- src/Server/ModuleLoader.py
+++ src/Server/ModuleLoader.py
@@ -8,18 +8,22 @@ Created on 2014/02/12 07:37:00
 import os
 import inspect
 
+def load_rules(module_name):
+    """Returns the functions in the module to be used in a Rule's script"""
+    get_functions(import_module(module_name))
+
 def import_module(module_name):
     """Searches the Variants directory for module_name and returns it"""
     assert type(module_name) == str
     try:
-        module = __import__("Rules." + module_name, 
+        module = __import__("Rules." + module_name,
                             fromlist = ["filler"])
                             #non-empty list imports module_name
     except ImportError, e:
-        raise (ImportError, 
-               "Could not find {} in {}".format(module_name, 
+        raise (ImportError,
+               "Could not find {} in {}".format(module_name,
                 os.getcwd() + os.sep + "Rules"))
-        
+
     return module
 
 def get_functions(module):
