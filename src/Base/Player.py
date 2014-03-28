1dd93964cd1c0e34400e72a8406c0d2ae7531ef8
Timidger <APragmaticPlace@gmail.com>
2014-03-28 08:28:08 -0500

    Player.py -> PEP8, changed get_card_index to get_card and added an in_hand method

--- src/Base/Player.py
+++ src/Base/Player.py
@@ -5,11 +5,12 @@ Created on Fri Dec 28 13:14:12 2012
 @author: Preston
 """
 
+
 class Player(object):
     """Creates a player that has a name, a hand of cards, and starts
     playing until he is removed from play either through a rule or when he
     runs out of cards"""
-    def __init__(self, name, hand = None, address = None):
+    def __init__(self, name, hand=None, address=None):
         """Creates a new player for a game which has a hand and name"""
         if hand is None:
             self.hand = []
@@ -17,24 +18,21 @@ class Player(object):
             self.hand = list(hand)
         self.name = name
 
-    def get_card_index(self, card):
-        """Returns the index of the card in the hand. Returns None if the
-        card cannot be found"""
-        rank_suit = (card.rank, card.suit)
-        for index, card in enumerate((card.rank, card.suit) for card in
-        self.hand):
-            if rank_suit == card:
-                return index
-        else:
-            return None
-
     def get_card(self, index):
-        "Pops the card out of the list and returns it"
+        """Returns and removes the card at the index"""
         return self.hand.pop(index)
 
-    def add_card(self, card, index = 0):
+    def in_hand(self, card):
+        """Returns true if the card is in the hand, false other wise"""
+        if card in self.hand:
+            return True
+        else:
+            return False
+
+    def add_card(self, card, index=0):
         "Add the card to the player's hand at the index (defaults to 0)"
         self.hand.insert(index, card)
 
     def __repr__(self):
-        return "Player named {} with {} cards".format(self.name, len(self.hand))
+        return "Player named {} with {} cards".format(self.name,
+                                                      len(self.hand))
