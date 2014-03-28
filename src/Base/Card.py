bcf813c67c633cf91b82b53d7a16b3f48c44cac6
Timidger <APragmaticPlace@gmail.com>
2014-03-28 10:12:31 -0500

    Added doc-strings, set bounds on the rank/suit set methods, and added __nonzero__ so that one can use if card

--- src/Base/Card.py
+++ src/Base/Card.py
@@ -4,20 +4,28 @@ Created on Fri Dec 28 13:12:58 2012
 
 @author: Preston
 """
+SUITS = "Hearts Clubs Spades Diamonds".split()
+RANKS = "Ace 2 3 4 5 6 7 8 9 10 Jack Queen King".split()
 
 
 class Card(object):
-    """A card is made up of a suit, rank, and is represented in the game by a
-    texture."""
+    """A card that has a suit and rank"""
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
     def change_suit(self, new_suit):
+        """Changes the suit of the card to the new suit"""
+        assert new_suit in SUITS
         self.suit = new_suit
 
     def change_rank(self, new_rank):
+        """Changes the rank of the card to the new rank"""
+        assert new_rank in RANKS
         self.rank = new_rank
+
+    def __nonzero__(self):
+        return any((self.rank, self.suit))
 
     def __eq__(self, other_card):
         return (other_card.rank, other_card.suit) == (self.rank, self.suit)
