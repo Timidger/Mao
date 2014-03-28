84c5075683c5e8fe7e86d84ea256d3f3aebe1ef6
Timidger <APragmaticPlace@gmail.com>
2014-03-27 20:13:43 -0500

    Fixed issue where I was importing the Module instead of the class

--- src/Client/Client.py
+++ src/Client/Client.py
@@ -8,9 +8,9 @@ import socket
 import pickle
 import threading
 import Queue
-from ..Base import Card
-from ..Base import Pile
-from ..Base import Player
+from ..Base.Card import Card
+from ..Base.Pile import Pile
+from ..Base.Player import Player
 
 
 class Client(object):
@@ -140,57 +140,3 @@ class Client(object):
 
     def __exit__(self, type, value, traceback):
         self.disconnect()
-
-if __name__ == '__main__':
-    ip = raw_input('ip (nothing for localname) = ')
-    port = int(raw_input('port = '))
-    name = raw_input('name (nothing for "Timidger") = ') or 'Timidger'
-    client = Client(port, ip, name)
-
-    def Client_listen():
-        while client.is_running():
-            try:
-                print client.message_queue.get(timeout = 1)
-            except Queue.Empty:
-                continue
-    threading.Thread(target = Client_listen).start()
-
-    while client.is_running():
-        message = raw_input()
-        if client.is_running():
-            if message.startswith('/'):
-                command = message.split('/', 1)[1].split()
-                if command[0] == 'pile':
-                    if client.pile.top_card.rank and (
-                    client.pile.top_card.suit):
-                        print client.pile.top_card.rank + ' of ' + (
-                        client.pile.top_card.suit)
-                    else:
-                        print 'No top card!'
-                elif command[0] == 'hand':
-                    for index, card in enumerate(client.player.hand):
-                        print str(index) + ': ' + (
-                        card.rank + ' of ' + card.suit)
-                elif command[0] == 'send' and command[1]:
-                    try:
-                        card = (client.player.get_card(int(command[1])))
-                        print card.rank + ' of ' + card.suit + ' sent!'
-                        client.send(card)
-                    except IndexError:
-                        print "There are only {} cards!".format(len(
-                        client.player.hand) + 1)
-                elif command[0] == 'draw':
-                    client.send(Card(None, None))
-                elif command[0] == 'help':
-                    print "Commands:"
-                    print "    pile: shows top card"
-                    print "    hand: shows hand"
-                    print "    send: send the card of the given index"
-                    print "    (0 == first, -1 == last)"
-                    print "    draw: draw a card....duh"
-                else:
-                    print "'/{}' is an invalid command!".format(' '.join(
-                                                         command))
-                print
-            else:
-                client.send(message)
