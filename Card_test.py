64b3c29b768f6a486f654936cfe9c4a5f9f2401d
Timidger <APragmaticPlace@gmail.com>
2014-05-18 22:27:54 -0500

    First Card image push. Looking good so far, just need to get transperency working, add the upside down rank, and make sure it works in tkinter

---
+++ Card_test.py
@@ -0,0 +1,15 @@
+import time
+import os
+from src.GUI.Card import Card as C
+from src.Base.Card import Card as Card
+
+ranks = "Ace 2 3 4 5 6 7 8 9 10 Jack Queen King".split()
+suits = "Hearts Spades Diamonds Clubs".split()
+
+deck = [Card(suit, rank) for rank in ranks for suit in suits]
+for card in deck:
+    C(card)
+    time.sleep(.1)
+
+time.sleep(3)
+os.system("killall display")
---
+++ src/GUI/Card.py
@@ -0,0 +1,34 @@
+import os
+from PIL import Image
+
+# Put this in the config
+image_directory = "Images/Cards/"
+IMAGE_EXT = ".gif"
+RANK_SIZE = (20, 20) # Size of the chunk that denotes a card's rank (x, y)
+TOP_CORDS = (3, 8, 23, 28) # Chunk above the top suit symbol
+BOTTOM_CORDS = ((128, 173), (128, 193)) # Chunk belove the bottom suit symbol
+
+class Card(object):
+    """Image representation of a card"""
+    def __init__(self, card):
+        self.card = card
+        rank, suit = card.rank, card.suit
+        image_file = self.make_card_image(card.rank, card.suit)
+        self.image = image_file
+
+    def make_card_image(self, rank, suit):
+        """Using the basic images that make up a card found in the image
+        directory, constructs a card from the given rank and suit"""
+        # Open a blank card corresponding to the given suit
+        blank_card = Image.open(image_directory + "blank_" + suit + IMAGE_EXT)
+        rank_chunk = Image.open(image_directory + "Ranks/" + rank + IMAGE_EXT)
+        print "Card: {}".format(blank_card)
+        print "chunk: {}".format(rank_chunk)
+        blank_card.paste(rank_chunk, TOP_CORDS)
+        # Still need bottom cords
+        blank_card.show() # Debug
+        return blank_card
+
+    def __repr__(self):
+        path = os.path.abspath(self.image.name)
+        return path
