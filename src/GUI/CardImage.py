99ada199d86c243c6826c22f4d27bffaf6fe2179
Timidger <APragmaticPlace@gmail.com>
2014-05-20 18:18:43 -0500

    Added a way to generate the coordinates for symbols to place on the cards

--- src/GUI/CardImage.py
+++ src/GUI/CardImage.py
@@ -10,11 +10,45 @@ IMAGE_EXT = ".gif"
 # Default symbol size: 20x20
 SYMBOL_SIZE = (20, 20) # Size of the chunk that denotes a card's rank (x, y)
 
+def get_rank_cords(card_size, symbol_size):
+    """Returns two lists representing the coordinates for the top and bottom
+    rank symbol on the card. This location is determined by taking a fraction
+    of the card_size using the symbol_size."""
+    # Get the top cords
+    x = card_size[0] // symbol_size[0]
+    y = card_size[1] // symbol_size[1]
+    top_cords = [x, y, symbol_size[0] + x, symbol_size[1] + y]
+
+    # Get the bottom cords, which is just a mirror of the top cords
+    x = card_size[0] - x
+    y = card_size[1] - y
+    # Since we start from the bottom, the first corrdinates have to be added
+    bottom_cords = [x - symbol_size[0], y - symbol_size[1], x, y]
+    return (top_cords, bottom_cords)
+
+def get_suit_cords(card_size, symbol_size, y_offset):
+    """Returns two tuples representing the coordinates for the top and bottom
+    suity symbol on the card. This operates exactly like get_rank_cords but the
+    offset is provided here so that the suit symbol can be below the rank
+    symbol"""
+    # The y_offset is based off the symbol's y-size
+    y_offset += symbol_size[1]
+    rank_cords = get_rank_cords(card_size, symbol_size)
+    rank_cords[0][1] += y_offset
+    rank_cords[0][3] += y_offset
+    # Now we do it again upside down
+    rank_cords[1][1] -= y_offset
+    rank_cords[1][3] -= y_offset
+    return rank_cords
+
+print(get_rank_cords(CARD_SIZE, SYMBOL_SIZE))
+print(get_suit_cords(CARD_SIZE, SYMBOL_SIZE, 5))
+
 RANK_TOP_CORDS = (7, 10, 27, 30) # Cords for the top rank symbol
 RANK_BOTTOM_CORDS = (118, 175, 138, 195) # Cords for the bottom rank symbol
 
-SUIT_TOP_CORDS = (7, 32, 27, 52) # Cords for the top suit symbol
-SUIT_BOTTOM_CORDS = (118, 155, 138, 175) # Cords for the bottom suit symbol
+SUIT_TOP_CORDS = (7, 35, 27, 55) # Cords for the top suit symbol
+SUIT_BOTTOM_CORDS = (118, 150, 138, 170) # Cords for the bottom suit symbol
 
 class CardImage(Tkinter.Button, object):
     """Image representation of a card"""
