6e90c9df3892e4164c455e5a533664ade6acd9ea
Timidger <APragmaticPlace@gmail.com>
2014-05-23 00:00:30 -0500

    Changed Pile into a full list subclass, removed the remove method (since it was just a wrapper for pop), and changed the magic number associated with the minmum deck length to half the product of the length of the ranks and suits.

--- src/Base/Pile.py
+++ src/Base/Pile.py
@@ -7,60 +7,47 @@ Created on Fri Dec 28 13:12:58 2012
 import random
 from .Card import Card, SUITS, RANKS
 
-MINIMUM_DECK_LENGTH = 26 # Half of a deck
+MINIMUM_DECK_LENGTH = (len(SUITS) * len(RANKS)) // 2 # Half of a deck
 
 
-class Pile(object):
+class Pile(list, object):
     """Creates a place for cards to be placed"""
-    def __init__(self, cards=None):
-        if cards is None:
-            self._cards = []
-        else:
-            self._cards = list(cards)
-            self.shuffle()
+    def __init__(self, *cards):
+        """Creates a new list with using cards"""
+        super(Pile, self).__init__(cards)
+        self.shuffle()
 
     def shuffle(self):  # Modern Fisher-Yates Shuffle Algorithm
         """Shuffles the deck"""
-        for first in range(len(self._cards) - 1, 0, -1):
+        for first in range(len(self) - 1, 0, -1):
             second = random.randint(0, first)
-            self._cards[second], self._cards[first] = (self._cards[first],
-                                                     self._cards[second])
+            self[second], self[first] = (self[first],
+                                                     self[second])
 
-    def draw(self, num_of_cards=1):
+    def draw(self):
         """Draws num_of_cards from the deck. If the deck gets low (<= 26)
         at any point, then a new deck is made, combined with the current deck,
         and then shuffled before drawing continues"""
-        cards = []
-        for card in range(num_of_cards):
-            # Add before the draw, in case there are not any cards left
-            if len(self._cards) <= MINIMUM_DECK_LENGTH:
-                new_cards = [Card(suit, rank) for rank in RANKS
-                                              for suit in SUITS]
-                self.add(new_cards)
-                self.shuffle()
-            card = self.remove()
-            cards.append(card)
-        return cards
+        # Add before the draw, in case there are not any cards left
+        if len(self) <= MINIMUM_DECK_LENGTH:
+            new_cards = [Card(suit, rank) for rank in RANKS
+                                          for suit in SUITS]
+            self.add(new_cards)
+            self.shuffle()
+        card = self.remove()
+        return card
 
     def add(self, card, index=0):
         """Adds the card to the pile at the appropriate index, which is the
         top card by default"""
-        assert(type(card) == Card)
-        self._cards.insert(index, card)
-
-    def remove(self, index=0):
-        """Removes and returns a card at the appropriate index"""
-        card = self._cards.pop(index)
-        return card
+        assert(isinstance(card, Card), "Tried to add a non-card!"
+        self.insert(index, card)
 
     def __getitem__(self, index):
-        if not self._cards and not index:
-            return Card(None, None)
+        if not self and not index:
+            return None
         else:
-            return self._cards[index]
-
-    def __len__(self):
-        return len(self._cards)
+            return super(Pile, self).__getitem__(index)
 
     def __repr__(self):
-        return "A Pile with {} cards".format(len(self._cards))
+        return "A Pile with {} cards".format(len(self))
